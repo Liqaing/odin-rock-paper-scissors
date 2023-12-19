bb41d8b3f542f67a2f18577272ea18ebf9ef71fa
Huot Rithy <huotrithy01@gmail.com>
2023-12-19 18:57:44 +0700

    Create function to prompt user for input
    In case of invalid input keep asking until input is correct or cancel

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,3 +3,22 @@ function getComputerChoice() {
     const random = Math.floor(Math.random() * 3); 
     return choices[random];
 }
+
+function getPlayerChoice() {
+    let isRunning = true;
+    let input;
+    while (isRunning) {
+        input = capitalize(prompt("Rock, Paper, Scissors? "));
+        if (input === "Rock" || input === "Paper" || input === "Scissors" || input == null) {
+            isRunning = false;
+        }
+    }
+    return input;
+}
+
+function capitalize(string) {
+    if (string === null) {
+        return;
+    }
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
